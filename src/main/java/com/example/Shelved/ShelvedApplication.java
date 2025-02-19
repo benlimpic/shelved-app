00f9cb928989d1ff54c06ae8ecf814a3a72e7efc
init.defaultBranch <benlimpic@gmail.com>
2025-02-19 10:00:42 -0800

    initial commit Springframework + Java + React.js

---
+++ src/main/java/com/example/Shelved/ShelvedApplication.java
@@ -0,0 +1,13 @@
+package com.example.Shelved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelvedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelvedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Shelved/ShelvedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Shelved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelvedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
