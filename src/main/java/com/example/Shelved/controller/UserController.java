6095466d1cd7921ab64ab906bd821306d2ada67e
init.defaultBranch <benlimpic@gmail.com>
2025-02-21 14:30:54 -0800

    updated usser controller to handle user and username validation and Get + Post + Put operations

--- src/main/java/com/example/Shelved/controller/UserController.java
+++ src/main/java/com/example/Shelved/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,10 +44,56 @@ public class UserController {
     userRepository.save(user);
   }
 
+  @PutMapping("/{username}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void updateUserInfo(@PathVariable("username") String username, @RequestBody User updatedUser) {
+
+    Optional<User> optionalExistingUser = this.userRepository.findByUsername(username);
+    if (optionalExistingUser.isPresent()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+    }
+
+    User existingUser = optionalExistingUser.get();
+
+    copyUserInfoFrom(updatedUser, existingUser);
+    userRepository.save(existingUser);
+  }
 
 
+  /*----------------------------------------------------------------------------------------- */
 
-/*----------------------------------------------------------------------------------------- */
+  
+  private void copyUserInfoFrom(User updatedUser, User existingUser) {
+
+    if (ObjectUtils.isEmpty(updatedUser.getUsername())) {
+      throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Username is required");
+    }
+
+    if (!ObjectUtils.isEmpty(updatedUser.getUserEmail())) {
+      existingUser.setUserEmail(updatedUser.getUserEmail());
+    }
+
+    if (!ObjectUtils.isEmpty(updatedUser.getUserPassword())) {
+      existingUser.setUserPassword(updatedUser.getUserPassword());
+    }
+
+    if (!ObjectUtils.isEmpty(updatedUser.getUserImage())) {
+      existingUser.setUserImage(updatedUser.getUserImage());
+    }
+
+    if (!ObjectUtils.isEmpty(updatedUser.getUserBio())) {
+      existingUser.setUserBio(updatedUser.getUserBio());
+    }
+
+    if (!ObjectUtils.isEmpty(updatedUser.getUserLocation())) {
+      existingUser.setUserLocation(updatedUser.getUserLocation());
+    }
+
+    if (!ObjectUtils.isEmpty(updatedUser.getUserBirthday())) {
+      existingUser.setUserBirthday(updatedUser.getUserBirthday());
+    }
+  }
+
 
   private void validateUser(User user) {
     validateUsername(user.getUsername());
